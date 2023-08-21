bc8ffa4632103f3f8b0b09348e1cef6de4b8f6a3
Ozan Gocmen <93273911+ozangocmen@users.noreply.github.com>
2023-08-21 11:06:17 +0300

    env creation

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Please enter the desired environment name:"
+read environment_name
+
+venv_path="$environment_name"
+
+if [ -d "$venv_path" ]; then
+    echo "Found an existing environment with the name '$environment_name'."
+    echo "Do you want to use the existing environment? (y/n)"
+    read use_existing
+
+    if [ "$use_existing" == "y" ]; then
+        source "$venv_path"/bin/activate
+        echo "Activated existing virtual environment '$environment_name'."
+    else
+        echo "Aborted."
+    fi
+else
+    python3 -m venv "$venv_path"
+    source "$venv_path"/bin/activate
+    echo "Virtual environment '$environment_name' has been created and activated."
+fi
+
+python setup.py
+
+python proton.py --pdb example-input/complex.pdb --chain_ID D --algorithm EvoEF1 --cut_off 5.0 --IQR 1.5
